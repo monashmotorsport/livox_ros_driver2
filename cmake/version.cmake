ca5bb9a5a61b2c814ddb6341238174b1000cb79e
kobe <kobe.nadji@monashmotorsport.com>
2025-08-24 20:11:15 +1000

    Removed ros1 cmake to prevent errors

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,7 +1,7 @@
 #---------------------------------------------------------------------------------------
 # Get livox_ros_driver2 version from include/livox_ros_driver2.h
 #---------------------------------------------------------------------------------------
-file(READ "${CMAKE_CURRENT_LIST_DIR}/../src/include/livox_ros_driver2.h" LIVOX_ROS_DRIVER2_VERSION_FILE)
+file(READ "${CMAKE_CURRENT_LIST_DIR}/../src/resources/include/livox_ros_driver2.h" LIVOX_ROS_DRIVER2_VERSION_FILE)
 string(REGEX MATCH "LIVOX_ROS_DRIVER2_VER_MAJOR ([0-9]+)" _  "${LIVOX_ROS_DRIVER2_VERSION_FILE}")
 set(ver_major ${CMAKE_MATCH_1})
 
